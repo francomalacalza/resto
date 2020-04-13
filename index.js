58998118468ac5393887e257a75c701bf86653a7
francomalacalza <malacalzafranco@gmail.com>
2020-04-13 01:02:16 -0300

    Verificacion de usuarios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const server = express()
 const bp = require('body-parser')
 const jwt = require('jsonwebtoken')
-const signature = 'mi_firma'
+const signature = 'my_secret'
 
 // CONEXION A BD
 const Sequelize = require('sequelize')
@@ -15,16 +15,18 @@ server.listen('3000', () => {
     console.log('Servidor iniciado')
 })
 
+
+// ENDPOINTS
 server.post('/signup', async (req, res, next) => {
     const newuser = req.body
     const existsUsername = await searchByParam('username', newuser.username)
     const existsEmail = await searchByParam('email', newuser.email)
 
-    if(existsUsername){
+    if(existsUsername.length > 0){
         res.status(409).json('Nombre de usuario en uso')
         return
     }
-    if(existsEmail){
+    if(existsEmail.length > 0){
         res.status(409).json('Ya existe un usuario registrado con ese email')
         return
     }
@@ -43,9 +45,69 @@ server.post('/signup', async (req, res, next) => {
     
 })
 
+server.get('/login', async (req, res, next) => {
+    const { username, password } = req.body
+    const existsUser = await searchByParam('username', username)
+    if(existsUser.length > 0){
+        if(existsUser[0].password === password){
+            const token = generateToken(existsUser[0])
+            res.status(200).json(token)
+        }else{
+            res.status(401).json('Clave incorrecta')
+        }
+    }else{
+        res.status(401).json('Usuario incorrecto')
+    }
+})
+
+server.get('/users', validateToken, async (req, res, next) =>{
+    const isAdmin = req.user.is_admin
+    console.log('aca llegue ' + isAdmin)
+    if(isAdmin == 0){
+        console.log('aca no tiene que entrar')
+        res.json(req.user)
+    }else{
+        const usersList = await searchTable('users')
+        const usersInfo = usersList.map((user) => {
+            delete user.password
+            return user
+        })
+        res.send(usersInfo)
+    }
+})
+
+// FUNCIONES
 async function searchByParam (filter, inputParam){
     const searchResult = await sequelize.query(`SELECT * FROM users WHERE ${filter} = ?`,
     { replacements: [inputParam], type: sequelize.QueryTypes.SELECT })
     console.log(searchResult)
-    return searchResult.length
+    return searchResult
+}
+async function searchTable (table){
+    const result = await sequelize.query(`SELECT * FROM ${table}`, { type: sequelize.QueryTypes.SELECT })
+    console.log(result)
+    return result
+}
+
+function generateToken(user){
+    return jwt.sign(user, signature)
+}
+
+function validateToken (req, res, next){
+    const autHeader = req.headers.authorization
+    if(autHeader){
+        const token = autHeader.split(' ')[1]
+        jwt.verify(token, signature, (err, data) =>{
+            if(data) {
+                console.log(data)
+                req.user = data
+                return next()
+            }else{
+                res.status(401).json('hay un error ' + err)
+            }
+        })
+    }else{
+        res.status(403).json({ error: 'No se ricibio un token' })
+    }
+
 }
