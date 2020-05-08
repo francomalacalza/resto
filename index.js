b9756f4ac20d3ceb93855c59d6c19c0d57cdf633
francomalacalza <malacalzafranco@gmail.com>
2020-05-07 23:07:01 -0300

    nuevos endpoints

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ server.listen('3000', () => {
 
 
 // ENDPOINTS
-server.post('/signup', async (req, res, next) => {
+server.post('/signup', async (req, res) => {
     const newuser = req.body
-    const existsUsername = await searchByParam('username', newuser.username)
-    const existsEmail = await searchByParam('email', newuser.email)
+    const existsUsername = await searchByParam('users', 'username', newuser.username)
+    const existsEmail = await searchByParam('users', 'email', newuser.email)
 
     if(existsUsername.length > 0){
         res.status(409).json('Nombre de usuario en uso')
@@ -30,14 +30,14 @@ server.post('/signup', async (req, res, next) => {
         res.status(409).json('Ya existe un usuario registrado con ese email')
         return
     }
-    if(newuser.username.trim() &&
-        newuser.fullname.trim() &&
-        newuser.password.trim() &&
-        newuser.email.trim() &&
-        newuser.phone.trim() &&
-        newuser.address.trim()
+    if(newuser.username &&
+        newuser.fullname &&
+        newuser.password &&
+        newuser.email &&
+        newuser.phone &&
+        newuser.address
         ){
-        sequelize.query('INSERT INTO users (username, fullname, password, email, phone, address, is_admin) VALUES (:username, :fullname, :password, :email, :phone, :address, 0)', {replacements: newuser }).then(rtdo => res.status(200).json('Usuario creado' + rtdo))
+        sequelize.query('INSERT INTO users (username, fullname, password, email, phone, address, isAdmin) VALUES (:username, :fullname, :password, :email, :phone, :address, 0)', {replacements: newuser }).then(rtdo => res.status(201).json('Usuario creado' + rtdo))
         
     }else{
         res.status(400).json('Falta uno o mas datos')
@@ -45,9 +45,9 @@ server.post('/signup', async (req, res, next) => {
     
 })
 
-server.get('/login', async (req, res, next) => {
+server.get('/login', async (req, res) => {
     const { username, password } = req.body
-    const existsUser = await searchByParam('username', username)
+    const existsUser = await searchByParam('users', 'username', username)
     if(existsUser.length > 0){
         if(existsUser[0].password === password){
             const token = generateToken(existsUser[0])
@@ -60,13 +60,12 @@ server.get('/login', async (req, res, next) => {
     }
 })
 
-server.get('/users', validateToken, async (req, res, next) =>{
-    const isAdmin = req.user.is_admin
+server.get('/users', validateToken, async (req, res) =>{
+    const isAdmin = req.user.isAdmin
     console.log('aca llegue ' + isAdmin)
-    if(isAdmin == 0){
-        console.log('aca no tiene que entrar')
+    if(isAdmin == 0){ //si no es admin solo ve su informacion
         res.json(req.user)
-    }else{
+    }else{ // el admin puede ver la info de todos, salvo el password
         const usersList = await searchTable('users')
         const usersInfo = usersList.map((user) => {
             delete user.password
@@ -76,13 +75,55 @@ server.get('/users', validateToken, async (req, res, next) =>{
     }
 })
 
+server.get('/products', validateToken, async(req, res) =>{
+    const productsList = await searchByParam('products', 'available', '1')
+    res.status(200).json(productsList)
+})
+
+server.post('/neworder', validateToken, async(req, res) =>{
+    const userId = req.user.userId
+    console.log(userId)
+    const { products, paymentMethod } = req.body
+    console.log(products)
+    let total = 0
+    products.forEach(async p => {
+        const price = await sequelize.query(`SELECT price FROM products WHERE productId = ${p.productId}`,
+        { type: sequelize.QueryTypes.SELECT })
+        console.log(price[0].price)
+        console.log(p.amount)
+        total += price[0].price * p.amount
+        console.log(total)
+    });
+    sequelize.query(`INSERT INTO orders (date, paymentMethod, total, status, userId) VALUES ('2020-07-02', '${paymentMethod}', ${total}, 'Confirmado', ${userId})`)
+    .then(rtdo => res.status(201).json('Orden creada'))
+})
+
+server.put('/order/:id', validateToken, async (req, res, next) => {
+    const isAdmin = req.user.isAdmin
+    const orderId = req.params.id;
+	const {orderStatus} = req.body;
+    if(isAdmin == 1){
+        const order = await searchByParam('orders', 'orderId', orderId)
+        if(order.length > 0){
+            sequelize.query(`UPDATE orders SET status = '${orderStatus}' WHERE orderId = ${orderId}`)
+            .then(rtdo => res.status(200).send('Estado actualizado'))
+        }else{
+            res.status(400).send('No existe pedido con el id elegido')
+        }
+    }else{
+        res.status(401).send('No tiene permisos')
+    }
+})
+
 // FUNCIONES
-async function searchByParam (filter, inputParam){
-    const searchResult = await sequelize.query(`SELECT * FROM users WHERE ${filter} = ?`,
+
+async function searchByParam (table, filter, inputParam){
+    const searchResult = await sequelize.query(`SELECT * FROM ${table} WHERE ${filter} = ?`,
     { replacements: [inputParam], type: sequelize.QueryTypes.SELECT })
     console.log(searchResult)
     return searchResult
 }
+
 async function searchTable (table){
     const result = await sequelize.query(`SELECT * FROM ${table}`, { type: sequelize.QueryTypes.SELECT })
     console.log(result)
